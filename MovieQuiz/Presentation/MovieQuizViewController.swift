fe47a6ba4a0656b25747c9b6ca4e16688091282e
Yandex Praktikum <praktikum@yandex.ru>
2024-01-21 14:24:47 +0300

    добавил код и сделал табуляцию

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -108,7 +108,10 @@ final class MovieQuizViewController: UIViewController {
             message: result.text,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
+            
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
@@ -118,7 +121,8 @@ final class MovieQuizViewController: UIViewController {
         }
         
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){ [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
         }
         
