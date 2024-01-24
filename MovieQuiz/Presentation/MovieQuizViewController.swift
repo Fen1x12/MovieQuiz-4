7e1a448d7d41f5e9437cdea9907bd6e70acfb082
Yandex Praktikum <praktikum@yandex.ru>
2024-01-24 21:56:12 +0300

    1

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -95,9 +95,24 @@ final class MovieQuizViewController: UIViewController {
             self.questionFactory.requestNextQuestion()
         }
     }
-
-   
     
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        let action = UIAlertAction(title: result.buttonText, style:  .default) { [weak self] _ in guard let self = self
+            else { return }
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            questionFactory.requestNextQuestion()
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated:  true, completion: nil)
+    }
+
     @IBAction private func yesBottunClecked(_ sender: Any) {
         let currentQuestion = questions[currentQuestionIndex]
         showAnswerResult(isCorrect: currentQuestion.correctAnswer == true)
