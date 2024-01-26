34a1625b1fac5708c1c73d2b4a97810195ec9b57
Yandex Praktikum <praktikum@yandex.ru>
2024-01-26 16:19:43 +0300

    добавления

---
+++ MovieQuiz/Models/GameRecord.swift
@@ -0,0 +1,8 @@
+//
+//  GameRecord.swift
+//  MovieQuiz
+//
+//  Created by  Admin on 26.01.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by  Admin on 26.01.2024.
+//
+
+import Foundation
